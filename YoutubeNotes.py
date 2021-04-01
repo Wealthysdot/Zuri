e15d3aa36b5aff53fe16f2965be2c6b7194a5309
Adedotun Sobambo <sobamboadedotun@gmail.com>
2021-04-01 09:56:55 +0100

    zuri Internship

---
+++ YoutubeNotes.py
@@ -0,0 +1,68 @@
+# print("Hello World")
+
+# name = "mike"
+# age = 55
+# List variable that can hold multiple value at the same time
+# with different data type
+# print("His name is %s and he is %d years old" % (name, age))
+
+# sampleList = ["mike", 'love', "dove"]
+# print("His name is  %s" % sampleList[1])
+
+# how to get string length
+# string = "I am a string"
+# print(len(string))
+
+# getting the position of a particular letter
+# print(string.index("r"))
+
+# getting a count of a string
+# print(string.count("n"))
+
+# printing from a particular location of the string
+# print(string[1:6])
+
+# changing letters to uppercase
+# print(string.upper())
+
+# changing to lower
+# print(string.lower())
+
+# reverse string
+# print(string[:: -1])
+
+# to print a part that starts with something
+# print(string.startswith("I"))
+
+
+# to print a part that starts with something
+# print(string.endswith("I"))
+
+
+firstValue = 4
+secondValue = 7
+# operators
+# They allow you perform actions on one or more variables
+# +, _, *, %
+
+# addition
+print(firstValue + secondValue)
+
+# subtraction
+print(firstValue - secondValue)
+
+# addition
+print(firstValue * secondValue)
+
+# division
+print(firstValue / secondValue)
+
+# modulus
+print(firstValue % secondValue)
+
+# assignment and comparison operators
+# assignment
+variable = 5
+
+# to compare if a one value is equal to the other
+print(firstValue == secondValue)
